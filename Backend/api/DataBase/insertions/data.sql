fdf9ef3bcba36181512b556b90e35b1f7abdaed3
Joffret kevin <joffretkevin727@gmail.com>
2026-03-16 12:52:41 +0100

    FEATURE : create troncage.html,modify schema.sql,clear and begin insertion

---
+++ Backend/api/DataBase/insertions/data.sql
@@ -0,0 +1,21 @@
+
+INSERT INTO roles (nom) VALUES
+    ('Assassin'),
+    ('Combattant'),
+    ('Mage'),
+    ('Tireur'),
+    ('Support'),
+    ('Tank');
+
+INSERT INTO champions (name, difficulte, description, price, stock, reduction, devise, genre, espece)
+VALUES
+    ('Aatrox',
+    'Difficile',
+    "Autrefois, Aatrox et ses frères étaient honorés pour avoir défendu Shurima contre le Néant. Mais ils finirent par devenir une menace plus grande encore pour Runeterra : la ruse et la sorcellerie furent employées pour les battre. Cependant, après des siècles d'emprisonnement, Aatrox fut le premier à retrouver sa liberté, en corrompant et transformant les mortels assez stupides pour tenter de s'emparer de l'arme magique qui contenait son essence. Désormais en possession d'un corps qu'il a approxim",
+    975,
+    50,
+    0.00,
+    'RP',
+    'Masculin', 
+    'darkin'),
+    (...)
